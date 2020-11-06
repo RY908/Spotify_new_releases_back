ea9c9cf040a85f92c25a87edf4d38774efd26a89
永井琉生 <ryusei@nagairyuseinoMacBook-puro.local>
2020-11-06 18:05:13 +0900

    add ifFollowing and add transaction

--- database/relation.go
+++ database/relation.go
@@ -9,20 +9,21 @@ import (
 )
 
 type ListenTo struct {
-	ListenId	int64 	`db:listenId`
-	ArtistId	string 	`db:artistId`
-	UserId		string 	`db:userId`
+	ListenId	int64 		`db:listenId`
+	ArtistId	string 		`db:artistId`
+	UserId		string 		`db:userId`
 	Timestamp	time.Time	`db:"timestamp"`
+	IfFollowing bool 		`db:"ifFollowing"`
 }
 
-func (d *MyDbMap) InsertRelation(artistId, userId string, timestamp time.Time) error {
+func (d *MyDbMap) InsertRelation(artistId, userId string, timestamp time.Time, ifFollowing bool) error {
 	count, err := d.DbMap.SelectInt("select count(*) from ListenTo where artistId = ? and userId = ?", artistId, userId)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
 	if count == 0 {
-		err = d.DbMap.Insert(&ListenTo{ArtistId:artistId, UserId: userId, Timestamp:timestamp})
+		err = d.DbMap.Insert(&ListenTo{ArtistId:artistId, UserId: userId, Timestamp:timestamp, IfFollowing: ifFollowing})
 	}
 	if err != nil {
 		fmt.Println(err)
@@ -31,12 +32,77 @@ func (d *MyDbMap) InsertRelation(artistId, userId string, timestamp time.Time) e
 	return nil
 }
 
+func (d *MyDbMap) InsertRelations(artists []ArtistInfo, userId string, timestamp time.Time, ifFollowing bool) error {
+	trans, err := d.DbMap.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	for _, artist := range artists {
+		artistId := artist.ArtistId
+		count, err := trans.SelectInt("select count(*) from ListenTo where artistId = ? and userId = ?", artistId, userId)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		if count == 0 {
+			err = trans.Insert(&ListenTo{ArtistId:artistId, UserId: userId, Timestamp:timestamp, IfFollowing: ifFollowing})
+		}
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+	}
+	return trans.Commit()
+}
+
 func (d *MyDbMap) DeleteRelation(userId string, timestamp time.Time) error {
-	res, err := d.DbMap.Exec("delete from ListenTo where userId = ? and timestamp <> ?", userId, timestamp)
+	res, err := d.DbMap.Exec("delete from ListenTo where userId = ? and timestamp <> ? and ifFollowing <> false", userId, timestamp)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
 	fmt.Println(res)
+	return nil
+}
+
+func (d *MyDbMap) UpdateFollowingRelation(artists []ArtistInfo, userId string, timestamp time.Time) error {
+	trans, err := d.DbMap.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	for _, artist := range artists {
+		artistId := artist.ArtistId
+		count, err := trans.SelectInt("select count(*) from ListenTo where artistId = ? and userId = ? and ifFollowing = true", artistId, userId)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		if count == 0 {
+			if err := trans.Insert(&ListenTo{ArtistId:artistId, UserId: userId, Timestamp:timestamp, IfFollowing: true}); err != nil {
+				return err
+			}
+		} else {
+			if _, err := trans.Update(&ListenTo{ArtistId:artistId, UserId: userId, Timestamp:timestamp, IfFollowing: true}); err != nil {
+				return err
+			}
+		}
+	}
+
+	if err := trans.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *MyDbMap) DeleteFollowingRelations(userId string, timestamp time.Time) error {
+	if _, err := d.DbMap.Exec("delete from ListenTo where userId = ? and timestamp = ? and ifFollowing = true", userId, timestamp); err != nil {
+		return err
+	}
+
 	return nil
 }
