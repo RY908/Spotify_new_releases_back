6dd23bbcffcb4d753104ad723f8f2ea620a3ad84
永井琉生 <ryusei@nagairyuseinoMacBook-puro.local>
2020-11-09 10:40:56 +0900

    add print

--- event/update.go
+++ event/update.go
@@ -11,6 +11,7 @@ import (
 
 // This is called every 50-60 minutes to get the users recently played tracks and insert the data into the database.
 func UpdateRelation(dbmap *MyDbMap) error {
+	fmt.Println("UpdateRelation")
 	// get all the users' information from database.
 	users, err := dbmap.GetAllUsers()
 
