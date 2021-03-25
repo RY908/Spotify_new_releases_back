5d274c3185cf538a0282937873a531843d2977be
Ryusei Nagai <ryuuryuu908@gmail.com>
2021-03-25 14:29:16 +0900

    add test database

--- backend/database/database_init.go
+++ backend/database/database_init.go
@@ -10,6 +10,7 @@ import (
 
 var (
 	sqlPath = os.Getenv("SQL_PATH")
+	sqlPathTest = os.Getenv("SQL_PATH_TEST")
 )
 
 type MyDbMap struct {
@@ -18,6 +19,23 @@ type MyDbMap struct {
 
 func DatabaseInit() (*MyDbMap, error) {
 	db, err := sql.Open("mysql", sqlPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %w", err)
+	}
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{}}
+	dbmap.AddTableWithName(ArtistInfo{}, "Artist").SetKeys(false, "ArtistId")
+	dbmap.AddTableWithName(ListenTo{}, "ListenTo").SetKeys(true, "ListenId")
+	dbmap.AddTableWithName(UserInfo{}, "User").SetKeys(false, "UserId")
+	//defer dbmap.Db.Close()
+	//defer db.Close()
+	//defer dbmap.Db.Close()
+
+	return &MyDbMap{DbMap: dbmap}, nil
+}
+
+func DatabaseTestInit() (*MyDbMap, error) {
+	fmt.Println(sqlPathTest)
+	db, err := sql.Open("mysql", sqlPathTest)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open database: %w", err)
 	}
